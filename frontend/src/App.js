3924d2e79196b9b520bbc6eedf955cee4f1e4966
rupatul1995 <163674583+rupatul1995@users.noreply.github.com>
2025-02-14 10:47:50 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes ,Route } from 'react-router-dom';
 import  Home from "./component/Home";
+// import Crud  from './component/Crud';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
      <Routes>
       <Route path='/' element={<Home/>}/>
+      {/* <Route path='crud' element={<Crud/>}/> */}
      </Routes>
     </div>
   );
--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,32 +1,197 @@
 function Home() {
+
+    
     return (
-        <div className="container mb-5">
-            <div className="row py-3">
-                <div className="col">
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                        <a className="navbar-brand" href="#">
-                            <img src="It_planteLogo.png" alt="Logo" style={{ height: '50px' }} />
-                        </a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav ms-auto">
-                                <li className="nav-item">
-                                    <a href="#" className="nav-link">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a href="#" className="nav-link">About</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a href="#" className="nav-link">Services</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
+          <div className="container-fluid mb-5">
+            <div className="row pt-2">
+              <div className="col">
+                <nav className="navbar navbar-expand-lg navbar-light bg-white py-2" style={{ width: '80%', margin: '0 auto' }}>
+                  <a href="" className="navbar-brand logo-dark">
+                    <img className="img-fluid" style={{ height: "40px" }} src="It_planteLogo.png" alt="Logo" />
+                  </a>
+        
+                  <ul className="navbar-nav mx-auto">
+                    <li className="nav-item"><a href="" className="nav-link">Home</a></li>
+                    <li className="nav-item"><a href="" className="nav-link">About Us</a></li>
+                    <li className="nav-item dropdown">
+                      <a href="" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Services</a>
+                      <div className="dropdown-menu">
+                        <a href=" " className="dropdown-item">Website Development</a>
+                        <a href=" " className="dropdown-item">App Development</a>
+                        <a href=" " className="dropdown-item">Video Production</a>
+                        <a href=" " className="dropdown-item">Web Hosting</a>
+                        <a href=" " className="dropdown-item">Digital Marketing</a>
+                        <a href=" " className="dropdown-item">Graphic Designing</a>
+                        <a className="dropdown-item">Printing Services</a>
+                      </div>
+                    </li>
+                    <li className="nav-item dropdown">
+                      <a href="" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Career</a>
+                      <div className="dropdown-menu">
+                        <a href=" " className="dropdown-item">Website Development</a>
+                        <a href=" " className="dropdown-item">App Development</a>
+                        <a href=" " className="dropdown-item">Video Production</a>
+                        <a href=" " className="dropdown-item">Web Hosting</a>
+                        <a href=" " className="dropdown-item">Digital Marketing</a>
+                        <a href=" " className="dropdown-item">Graphic Designing</a>
+                        <a className="dropdown-item">Printing Services</a>
+                      </div>
+                    </li>
+                    <li className="nav-item"><a href="" className="nav-link">Our Clients</a></li>
+                    <li className="nav-item"><a href="" className="nav-link">Contact Us</a></li>
+                  </ul>
+        
+                  <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                      <a href="tel:+9766397512" className="nav-link text-light d-flex align-items-center" style={{ backgroundColor: 'black', padding: '5px 15px', borderRadius: '20px' }}>
+                        <i className="fas fa-phone-alt" style={{ marginRight: '8px' }}></i> +91 9766397512
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+        
+            <div class="col-lg-12">
+              <img class="img-fluid w-100" src="website-developement-in-vashi.png" alt="Website Development" />
+            </div>
+        
+        
+           
+        <div class="container">
+             <h2 class="" style={{fontSize:60}}>Explore Our Services</h2>
+            <div class="row py-5">
+                <div class="col-md-4 pb-5">
+                <div class="card">
+               <center><img src="layout.gif" class="col-4 img-fluid"   />
+               </center>                 
+               <div class="card-body">
+                    <h4 class="card-title">Website Development</h4>
+                    <p class="card-text">
+                    Transform your online presence with custom-built websites that showcase your brand and engage your audience.</p>
+                    <button class="btn btn-primary">Read more</button>
                 </div>
             </div>
+             </div>
+             
+             <div class="col-md-4 pb-5">
+                <div class="card">
+                <center><img src="online-shopping.gif" class="col-4 img-fluid"   />
+               </center> 
+                <div class="card-body">
+                    <h4 class="card-title">Website Development</h4>
+                    <p class="card-text">
+                    Transform your online presence with custom-built websites that showcase your brand and engage your audience.</p>
+                    <button class="btn btn-primary">Read more</button>
+                </div>
+            </div>
+            </div>
+           
+            <div class="col-md-4">
+                <div class="card">
+                <center><img src="mobile-apps.gif" class="col-4 img-fluid"   />
+                </center> 
+                <div class="card-body">
+                    <h4 class="card-title">Website Development</h4>
+                    <p class="card-text">
+                    Transform your online presence with custom-built websites that showcase your brand and engage your audience.</p>
+                    <button class="btn btn-primary">Read more</button>
+                </div>
+            </div>
+            </div>
+        
+            <div class="col-md-4">
+                <div class="card">
+                <center><img src="data-cloud.gif" class="col-4 img-fluid"   />
+                </center>                 
+                <div class="card-body">
+                    <h4 class="card-title">Website Development</h4>
+                    <p class="card-text">
+                    Transform your online presence with custom-built websites that showcase your brand and engage your audience.</p>
+                    <button class="btn btn-primary">Read more</button>
+                </div>
+            </div>
+            </div>
+        
+            <div class="col-md-4 pb-5">
+                <div class="card">
+                <center><img src="promotion.gif" class="col-4 img-fluid"   />
+                </center> 
+                <div class="card-body">
+                    <h4 class="card-title">Website Development</h4>
+                    <p class="card-text">
+                    Transform your online presence with custom-built websites that showcase your brand and engage your audience.</p>
+                    <button class="btn btn-primary">Read more</button>
+                </div>
+            </div>
+            </div>
+            
+        
+            <div class="col-md-4 pb-5">
+                <div class="card">
+                <center><img src="logo-design.gif" class="col-4 img-fluid"   />
+                </center> 
+                <div class="card-body">
+                    <h4 class="card-title">Website Development</h4>
+                    <p class="card-text">
+                    Transform your online presence with custom-built websites that showcase your brand and engage your audience.</p>
+                    <button class="btn btn-primary">Read more</button>
+                </div>
+            </div>
+            </div>
+            
+        
+            <div class="col-md-4 pb-5">
+                <div class="card">
+                <center><img src="printer.gif" class="col-4 img-fluid"   />
+                </center> 
+                <div class="card-body">
+                    <h4 class="card-title">Website Development</h4>
+                    <p class="card-text">
+                    Transform your online presence with custom-built websites that showcase your brand and engage your audience.</p>
+                    <button class="btn btn-primary">Read more</button>
+                </div>
+            </div>
+            </div>
+        
+        
+        
+            <div class="col-md-4 pb-5">
+                <div class="card">
+                <center><img src="film-shooting.gif" class="col-4 img-fluid"   />
+                </center> 
+                <div class="card-body">
+                    <h4 class="card-title">Website Development</h4>
+                    <p class="card-text">
+                    Transform your online presence with custom-built websites that showcase your brand and engage your audience.</p>
+                    <button class="btn btn-primary">Read more</button>
+                </div>
+            </div>
+            </div>
+        
+
+        <div class="partnership-area">
+            <div class="container"> 
+         <div class="row mb-40">
+            <div class="col-lg-12">
+                <div class="section-title">
+                <h2>Our Happy <span> Clients</span></h2></div>
+            </div>
+
+           </div>
+            </div>
         </div>
+
+
+
+
+
+
+
+
+            </div>
+          </div>
+          </div>
     );
 }
 
